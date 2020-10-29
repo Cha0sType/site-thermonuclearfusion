516010358de4ec3a6bc13d59ca0100dac7425b82
Cha0sType <chaostypespam@gmail.com>
2020-10-29 09:44:41 -0700

    Created basic design of the website

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/iter')
+def iter():
+    return "Iter side in progress"
+
+@app.route('/basics')
+def basics():
+    return "Basics of nuclear fusion reactor side in progress"
+
+@app.route('/reactors')
+def reactors():
+    return "Reactors side in progress"
+
+if __name__ == '__main__':
+    app.run(port=1234, debug=True, threaded=True)
