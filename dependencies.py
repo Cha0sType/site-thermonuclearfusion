5460aa56269d2dd99f304f17d3c96817208c7ac0
ChaosType <chaostype@chaos-desktop-linux.fritz.box>
2020-11-10 23:08:57 +0100

    Commented dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,10 +1,11 @@
 def openDependencies():
-    header_html = open('templates/header.html')
-    header = header_html.read()
-    header_html.close()
+    header_html = open('templates/header.html') #Opening the header html file
+    header = header_html.read() #Reading the file
+    header_html.close() #Closing the file
 
-    icon_svg = open('static/Icon.svg')
-    icon = icon_svg.read()
-    icon_svg.close()
-    opend = [header, icon]
-    return opend
+    icon_svg = open('static/Icon.svg') #Opening the icon
+    icon = icon_svg.read() #Reading the file
+    icon_svg.close() #Closing the file
+
+    opend = [header, icon] #Putting the both opend items into an array for better handling
+    return opend #Returning the array to make it accessible outside of the function
