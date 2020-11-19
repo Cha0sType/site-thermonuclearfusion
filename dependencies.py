07c24eac58c864fec178909d7a9f953a6c79608c
cha0stype <chaostypespam@gmail.com>
2020-11-19 20:02:08 +0100

    Removed searchbar and fixed a bug with utf-8 encoding

--- dependencies.py
+++ dependencies.py
@@ -1,13 +1,13 @@
 def openDependencies():
-    header_html = open('templates/header.html') #Opening the header html file
+    header_html = open('templates/header.html', encoding="utf-8") #Opening the header html file
     header = header_html.read() #Reading the file
     header_html.close() #Closing the file
 
-    footer_html = open('templates/footer.html')
+    footer_html = open('templates/footer.html', encoding="utf-8")
     footer = footer_html.read()
     footer_html.close()
 
-    icon_svg = open('static/Icon.svg') #Opening the icon
+    icon_svg = open('static/Icon.svg', encoding="utf-8") #Opening the icon
     icon = icon_svg.read() #Reading the file
     icon_svg.close() #Closing the file
 
